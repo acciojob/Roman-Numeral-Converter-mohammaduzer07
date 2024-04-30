46dbdb1de8348d5a2d16b9e7af86f33ae6a48d86
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-04-30 16:40:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 function convertToRoman(num) {
-  	// const obj = {
-   //    0:['M',1000], 
-   //    1:['D', 500], 
-   //    2:['C', 100], 
-   //    3:['L', 50], 
-   //    4:['X', 10], 
-   //    5:['V', 5], 
-   //    6:['I', 1]
-   //  };
+  	const obj = {
+      0:['M',1000], 
+      1:['D', 500], 
+      2:['C', 100], 
+      3:['L', 50], 
+      4:['X', 10], 
+      5:['V', 5], 
+      6:['I', 1]
+    };
 
   //your code here
-	const values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+	// const values = [1000, 500, 100, 50, 10, 5, 1];
 	// const symbol = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
-	const symbol = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-	let ans = "";
+
+	const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+	const symbol = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+	
+	let ans = '';
 	for(let i=0; i<values.length; i++){
 		while(num >= values[i]){
 			ans += symbol[i];
@@ -25,20 +28,9 @@ function convertToRoman(num) {
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// Get the necessary DOM elements
-const numberInput = document.getElementById("number-input");
-const convertButton = document.getElementById("convert-button");
-const resultDiv = document.getElementById("result");
-
-// Add click event listener to the convert button
-convertButton.addEventListener("click", () => {
-  const inputNumber = parseInt(numberInput.value, 10);
-  const romanNumeral = convertToRoman(inputNumber);
-  resultDiv.textContent = `Roman Numeral: ${romanNumeral}`;
-});
 console.log(convertToRoman(36));
 
 
 
 // do not edit below this line
-// module.exports = convertToRoman
+module.exports = convertToRoman
